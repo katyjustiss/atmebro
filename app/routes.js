8118238a12ee5d5fb4bb622e2c74acfdf1a6dd78
Katy Justiss <katy.justiss@gmail.com>
2015-08-20 10:56:04 -0500

    added image route

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,9 @@ var express = require('express');
 var router = express.Router();
 
 var home = require('./home/routes');
+var image = require('./imageUpload/routes');
 
 router.use('/', home);
+router.use('/image', image);
 
 module.exports = router;
