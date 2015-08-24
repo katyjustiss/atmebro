dd22c7f099da2f972a128b5f6da5a8c79f2cdc73
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-08-24 10:23:59 -0500

    fixing travis issues

--- app/imageUpload/image.test.js
+++ app/imageUpload/image.test.js
@@ -1,17 +1,18 @@
+"use strict";
 var expect = require('chai').expect;
 var request = require('supertest');
 
 var app = require('../../app/');
 
 describe('Valid form', function (){
-  it('respond with profile', function(done){
+  it('respond with profile', function (done){
     request(app)
       .get('/image')
       .expect(200)
       .end(function (err, res) {
         if (err) throw err;
         // console.log(res)
-        expect(res.text).to.contain("multipart/form-data");
+        expect(res.text).to.contain('multipart/form-data');
         done();
       });
   });
