320a695f5e8d3aa7e3769715d88bcded2f903e13
Katy Justiss <katy.justiss@gmail.com>
2015-09-07 16:11:48 -0500

    image tests

--- app/imageUpload/image.test.js
+++ app/imageUpload/image.test.js
@@ -1,28 +1,47 @@
-'use strict';
-var expect = require('chai').expect;
-var request = require('supertest');
-
-var app = require('../../app/');
-
-describe('Valid form', function (){
-  it('respond with profile', function (done){
-    request(app)
-      .get('../post/index')
-      .expect(200)
-      .end(function (err, res) {
-        if (err) throw err;
-        // console.log(res)
-        expect(res.text).to.contain('multipart/form-data');
-        done();
-      });
-  });
-});
-
-// describe('Image validation', function() {
-//   it('file is <= 100000', function () {
-//     expect(req.file.size).to.be.below(100000);
-//   });
-//   it('errors if file size > 100000'){
-//     expect(res.err).to.equal('Image is larger than 10MB. Make the image smaller and then try uploading again.')
-//   }
-// });
++'use strict';
++var expect = require('chai').expect;
++var request = require('supertest');
++
++var app = require('../../app/');
++var mongo = require('../../lib/mongo');
++var Post = require('../post/Post');
++
++describe.only('IMAGE UPLOAD /post', function () {
++  var post;
++
++  before(mongo.connect);
++  after(Post.dropCollection);
++
++  it('should create a post with an image', function (done) {
++    Post.findAll(function(err, posts) {
++      expect(posts).to.deep.equal([]);
++
++      request(app)
++        .post('/post')
++        .attach('image', 'app/imageUpload/fixtures/cat.jpg')
++        .expect(302)
++        .expect('Moved Temporarily. Redirecting to /')
++        .end(function (err) {
++          if (err) throw err;
++          Post.findAll(function(err, posts) {
++            post = posts[0];
++            expect(posts.length).to.equal(1);
++            expect(post.url).to.exist;
++            done();
++          });
++        });
++    });
++  });
++
++  it('should create a post with an image', function (done) {
++    request(app)
++      .get(`/post/${post._id}`)
++      .expect(200)
++      .end(function (err, res) {
++        if (err) throw err;
++        expect(res.text).to.contain(`<img src="${post.url}" class="post">`);
++        done();
++      });
++  });
++
++});
